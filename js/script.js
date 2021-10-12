007b89ce5d18e708eab2d4bf1075bcce4d28c693
Danny <danny.brogno@gmail.com>
2021-10-12 17:59:40 +0200

    transformation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-
+// function parallax() {
+//
+//   $("window").scroll(function() {
+//
+//     let bg = $("#bg");
+//     let moon = $("#moon");
+//     let mountain = $("#mountain");
+//     let road = $("#road");
+//     let text = $("#text");
+//
+//     let value = window.scrollY;
+//
+//     bg.style.top = value * 0.5 + "px";
+//     moon.style.left = -value * 0.5 + "px";
+//     mountain.style.top = -value * 0.15 + "px";
+//     road.style.top = value * 0.15 + "px";
+//     text.style.top = value * 1 + "px";
+//
+//   });
+//
+// }
+//
+// function init () {
+//   parallax();
+// };
+//
+// $(document).ready(init);
